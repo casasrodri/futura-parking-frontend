849f44bdd6266f2e033f5e0b90671702ee223df9
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-10-01 03:19:04 -0300

    avances gui

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RegistroUsuarioVista from '../views/RegistroUsuarioVista.vue'
+import PrincipalVista from '../views/PrincipalVista.vue'
+import InicioSesionVista from '../views/InicioSesionVista.vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/registro',
+      name: 'signup',
+      component: RegistroUsuarioVista
     },
+    {
+      path: '/',
+      name: 'principal',
+      component: PrincipalVista,
+      children: [
+        {
+          path: '',
+          redirect: 'home'
+        },
+        {
+          path: 'home',
+          name: 'home',
+          component: HomeView
+        }
+      ]
+    },
+    {
+      path: '/ingresar',
+      name: 'login',
+      component: InicioSesionVista
+    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     {
       path: '/about',
       name: 'about',
