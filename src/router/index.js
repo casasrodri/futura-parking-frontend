5ceff3efb0ecb373828250296664f39fcc4cc776
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-10-01 03:22:01 -0300

    elimino about

--- src/router/index.js
+++ src/router/index.js
@@ -19,10 +19,10 @@ const router = createRouter({
       children: [
         {
           path: '',
-          redirect: 'home'
+          redirect: 'app'
         },
         {
-          path: 'home',
+          path: 'app',
           name: 'home',
           component: HomeView
         }
@@ -32,20 +32,20 @@ const router = createRouter({
       path: '/ingresar',
       name: 'login',
       component: InicioSesionVista
-    },
+    }
     // {
     //   path: '/',
     //   name: 'home',
     //   component: HomeView
     // },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
