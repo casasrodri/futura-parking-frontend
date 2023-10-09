0a5f6f7ccb9ffb6ffa646e1ad97718606248208d
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-10-09 01:16:18 -0300

    mostrado de chats

--- src/views/conversaciones/ConversacionesList.vue
+++ src/views/conversaciones/ConversacionesList.vue
@@ -1,6 +1,20 @@
 <template>
     <div v-for="conv in conversaciones" :key="conv._id">
-        {{ conv }}
+        <RouterLink :to="{ path: `/conversaciones/${conv._id}` }">
+            <div v-if="conv.oferente._id != localUser().id">
+                Usuario: {{ conv.oferente.nombre }} {{ conv.oferente.apellido }}
+            </div>
+            <div v-if="conv.demandante._id != localUser().id">
+                Usuario: {{ conv.demandante.nombre }} {{ conv.demandante.apellido }}
+            </div>
+            <div>
+                Publicacion: ...{{ conv.publicacion._id.slice(-4) }}
+            </div>
+            <div>
+                Mensajes no leídos: {{ conv.noLeidos }}
+            </div>
+        </RouterLink>
+        <hr>
     </div>
 </template>
 
@@ -12,8 +26,17 @@ import localUser from '../../utils/localUser.js';
 const conversaciones = ref()
 
 onMounted(async () => {
-    const convs = await Conversacion.obtenerDelUsuario(localUser().id)
-    conversaciones.value = convs.data
+    const miUsuario = localUser().id
+    const { data } = await Conversacion.obtenerDelUsuario(miUsuario)
+
+    // FIXME Me está trayendo todos los mensajes del chat
+    for (const conversacion of data) {
+        console.log(conversacion._id, miUsuario)
+        const { data: noLeidos } = await Conversacion.contarMensajesNoLeidos(conversacion._id, miUsuario)
+        conversacion.noLeidos = noLeidos.length
+    }
+
+    conversaciones.value = data
 })
 
 </script>
